0917a18628bff38e12c7ee85e6527d4eeba14371
Spencer Olson <spence1818@gmail.com>
2016-05-01 16:34:17 -0500

    set up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount Knock::Engine => "/knock"
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
