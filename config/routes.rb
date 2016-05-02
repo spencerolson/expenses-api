4c3293ddee5a2789101bd4aadb6d1eaa25ecd08c
Spencer Olson <spence1818@gmail.com>
2016-05-01 21:34:41 -0500

    add user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Knock::Engine => "/knock"
-
   post '/users' => 'users#create'
+  get '/users' => 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
